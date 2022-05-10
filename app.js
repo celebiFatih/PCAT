d9c436df4d77414bfc69b8c858c1d8e03ae83069
fatih celebi <fatihcelebi13@gmail.com>
2022-05-10 23:12:37 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
-
+const req = require('express/lib/request');
+const path = require('path');
 const app = express();
 
+// // mylogger middleware
+// const myLogger = (req, res, next) => {
+//   console.log("Middleware log 1");
+//   next() // bir sonraki middleware ilerlemesi için
+// }
+// //mylogger2 middleware
+// const myLogger2 = (req, res, next) => {
+//   console.log("Middleware log 2");
+//   next() // bir sonraki middleware ilerlemesi için
+// }
+
+//STATIC FILES -- MIDDLEWARES
+app.use(express.static('public')) //middleware'i kullanmak için use fonk kullanılır
+// app.use(myLogger); // myLogger middleware çağırdık
+// app.use(myLogger2); // myLogger2 middleware çağırdık
+
 app.get('/', (req, res) => {
-    const photo = {
-        id: 1,
-        name: "Photo Name",
-        desc: "Photo description"
-    }
-    res.send(photo)
+    res.sendFile(path.resolve(__dirname, 'temp/index.html'))
 });
 
 const port = 3000;
